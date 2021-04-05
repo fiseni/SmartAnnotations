8c39a7b43b016c0cc8750b84e2632ac790a6caa5
Fati Iseni <fati.iseni@pozitrongroup.com>
2021-04-05 21:49:24 +0200

    Added tests for DisplayAttribute generators.

---
+++ tests/SmartAnnotations.UnitTests/DisplayAttribute/DisplayAttributeGenerator_GetContent.cs
@@ -0,0 +1,55 @@
+﻿using FluentAssertions;
+using SmartAnnotations.DisplayAttribute;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace SmartAnnotations.UnitTests.DisplayAttribute
+{
+    public class DisplayAttributeGenerator_GetContent
+    {
+        private class TestResource { }
+        public string? TestProperty { get; set; } = null;
+
+        [Fact]
+        public void ReturnsFullContent_GivenAllParameters()
+        {
+            var descriptor = new DisplayAttributeDescriptor(typeof(TestResource))
+            {
+                Order = 5,
+                AutoGenerateField = true,
+                AutoGenerateFilter = true,
+                Name = "SomeName",
+                ShortName = "SomeShortName",
+                Prompt = "SomePrompt",
+                Description = "SomeDescription",
+                GroupName = "SomeGroupName",
+            };
+            var context = new AnnotationDescriptor(nameof(TestProperty), typeof(string)) { Display = descriptor };
+
+            var generator = new DisplayAttributeGenerator(context);
+
+            var expected = $"[Display(Order = 5, AutoGenerateField = true, AutoGenerateFilter = true, Name = \"SomeName\", " +
+                $"ShortName = \"SomeShortName\", Prompt = \"SomePrompt\", Description = \"SomeDescription\", " +
+                $"GroupName = \"SomeGroupName\", ResourceType = typeof(TestResource))]";
+
+            generator.GetContent().Should().Be(expected);
+        }
+
+        [Fact]
+        public void ReturnsMinimalContent_GivenNoParameters()
+        {
+            var descriptor = new DisplayAttributeDescriptor();
+            var context = new AnnotationDescriptor(nameof(TestProperty), typeof(string)) { Display = descriptor };
+
+            var generator = new DisplayAttributeGenerator(context);
+
+            var expected = "[Display()]";
+
+            generator.GetContent().Should().Be(expected);
+        }
+    }
+}
---
+++ tests/SmartAnnotations.UnitTests/DisplayAttribute/PromptGenerator_GetContent.cs
@@ -0,0 +1,36 @@
+﻿using FluentAssertions;
+using SmartAnnotations.DisplayAttribute;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace SmartAnnotations.UnitTests.DisplayAttribute
+{
+    public class PromptGenerator_GetContent
+    {
+        [Fact]
+        public void ReturnsContent_GivenPromptHasValue()
+        {
+            var descriptor = new DisplayAttributeDescriptor() { Prompt = "SomePrompt" };
+            var generator = new PromptGenerator(descriptor);
+
+            var expected = @"Prompt = ""SomePrompt""";
+
+            generator.GetContent().Should().Be(expected);
+        }
+
+        [Fact]
+        public void ReturnsEmptyContent_GivenPromptIsNull()
+        {
+            var descriptor = new DisplayAttributeDescriptor() { Prompt = null };
+            var generator = new PromptGenerator(descriptor);
+
+            var expected = string.Empty;
+
+            generator.GetContent().Should().Be(expected);
+        }
+    }
+}
