1b43cdcee3ca295079829f9f03e91b850a995768
Fati Iseni <fati.iseni@pozitrongroup.com>
2021-04-06 17:34:50 +0200

    Added tests.

---
+++ tests/SmartAnnotations.UnitTests/AnnotationContextTests.cs
@@ -0,0 +1,28 @@
+﻿using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace SmartAnnotations.UnitTests
+{
+    public class AnnotationContextTests
+    {
+        [Fact]
+        public void ThrowsArgumentNullException_GivenNullInConstructor()
+        {
+            Action action = () => new TestAnnotator();
+
+            action.Should().Throw<ArgumentNullException>().And.ParamName.Should().Be("type");
+        }
+
+        private class TestAnnotator : AnnotationContext
+        {
+            public TestAnnotator() : base(null!)
+            {
+            }
+        }
+    }
+}
---
+++ tests/SmartAnnotations.UnitTests/ContextBuilderExtensions_ResourceType.cs
@@ -0,0 +1,50 @@
+﻿using FluentAssertions;
+using SmartAnnotations.Internal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace SmartAnnotations.UnitTests.DisplayAttribute
+{
+    public class ContextBuilderExtensions_ResourceType
+    {
+        [Fact]
+        public void SetsModelResourceType_GivenNotNullValue()
+        {
+            var context = new TestAnnotator();
+
+            var contextBuilder = new ContextBuilder(context);
+
+            contextBuilder.ResourceType(typeof(ModelTestResource));
+
+            contextBuilder.Context.Should().NotBeNull();
+            contextBuilder.Context.ResourceType.Should().NotBeNull();
+            contextBuilder.Context.ResourceType.Should().Be(typeof(ModelTestResource));
+        }
+
+        [Fact]
+        public void ThrowsArgumentNullException_GivenNull()
+        {
+            var context = new TestAnnotator();
+
+            var contextBuilder = new ContextBuilder(context);
+
+            Func<IContextBuilder> func = () => contextBuilder.ResourceType(null!);
+
+            func.Should().Throw<ArgumentException>().And.ParamName.Should().Be("resourceType");
+        }
+
+
+        private class ModelTestResource { }
+        private partial class TestType
+        {
+            public string? TestProperty { get; set; } = null;
+        }
+        private class TestAnnotator : Annotator<TestType>
+        {
+        }
+    }
+}
