f86ea2ece30a962ccfe79beffdea8badaa431510
Fati Iseni <fati.iseni@pozitrongroup.com>
2021-04-06 12:07:39 +0200

    Added test for validation attribute base generators.

---
+++ tests/SmartAnnotations.UnitTests/ValidationAttribute/ErrorMessageGenerator_GetContent.cs
@@ -0,0 +1,39 @@
+﻿using FluentAssertions;
+using SmartAnnotations.ValidationAttribute;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace SmartAnnotations.UnitTests.ValidationAttribute
+{
+    public class ErrorMessageGenerator_GetContent
+    {
+        private class AttributeTestResource { }
+        private class ModelTestResource { }
+
+        [Fact]
+        public void ReturnsContent_GivenErrorMessage()
+        {
+            var descriptor = new ValidationAttributeDescriptor(typeof(AttributeTestResource)) { ErrorMessage = "SomeErrorMessage" };
+            var generator = new ErrorMessageGenerator(descriptor);
+
+            var expected = @"ErrorMessage = ""SomeErrorMessage""";
+
+            generator.GetContent().Should().Be(expected);
+        }
+
+        [Fact]
+        public void ReturnsEmptyContent_GivenNullErrorMessage()
+        {
+            var descriptor = new ValidationAttributeDescriptor(typeof(AttributeTestResource));
+            var generator = new ErrorMessageGenerator(descriptor);
+
+            var expected = string.Empty;
+
+            generator.GetContent().Should().Be(expected);
+        }
+    }
+}
---
+++ tests/SmartAnnotations.UnitTests/ValidationAttribute/ResourceTypeGenerator_GetContent.cs
@@ -0,0 +1,72 @@
+﻿using FluentAssertions;
+using SmartAnnotations.ValidationAttribute;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace SmartAnnotations.UnitTests.ValidationAttribute
+{
+    public class ResourceTypeGenerator_GetContent
+    {
+        private class AttributeTestResource { }
+        private class ModelTestResource { }
+
+        [Fact]
+        public void ReturnsAttributeResourceTypeContent_GivenHasAttributeAndModelResourceTypeAndHasErrorMessageResourceName()
+        {
+            var descriptor = new ValidationAttributeDescriptor(typeof(AttributeTestResource), typeof(ModelTestResource)) { ErrorMessageResourceName = "SomeResourceKey" };
+            var generator = new ResourceTypeGenerator(descriptor);
+
+            var expected = @"ErrorMessageResourceType = typeof(AttributeTestResource)";
+
+            generator.GetContent().Should().Be(expected);
+        }
+
+        [Fact]
+        public void ReturnsAttributeResourceTypeContent_GivenHasAttributeResourceTypeAndHasErrorMessageResourceName()
+        {
+            var descriptor = new ValidationAttributeDescriptor(typeof(AttributeTestResource)) { ErrorMessageResourceName = "SomeResourceKey" };
+            var generator = new ResourceTypeGenerator(descriptor);
+
+            var expected = @"ErrorMessageResourceType = typeof(AttributeTestResource)";
+
+            generator.GetContent().Should().Be(expected);
+        }
+
+        [Fact]
+        public void ReturnsModelResourceTypeContent_GivenHasModelResourceTypeAndHasErrorMessageResourceName()
+        {
+            var descriptor = new ValidationAttributeDescriptor(null, typeof(ModelTestResource)) { ErrorMessageResourceName = "SomeResourceKey" };
+            var generator = new ResourceTypeGenerator(descriptor);
+
+            var expected = @"ErrorMessageResourceType = typeof(ModelTestResource)";
+
+            generator.GetContent().Should().Be(expected);
+        }
+
+        [Fact]
+        public void ReturnsEmptyContent_GivenHasNoResourceType()
+        {
+            var descriptor = new ValidationAttributeDescriptor();
+            var generator = new ResourceTypeGenerator(descriptor);
+
+            var expected = string.Empty;
+
+            generator.GetContent().Should().Be(expected);
+        }
+
+        [Fact]
+        public void ReturnsEmptyContent_GivenHasResourceTypeHasNoErrorMessageResourceName()
+        {
+            var descriptor = new ValidationAttributeDescriptor(typeof(AttributeTestResource));
+            var generator = new ResourceTypeGenerator(descriptor);
+
+            var expected = string.Empty;
+
+            generator.GetContent().Should().Be(expected);
+        }
+    }
+}
